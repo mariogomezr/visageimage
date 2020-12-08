132d0e329deaf272ebc90a1d95c389ca9f85226c
Nicodez <n98hernandezv@gmail.com>
2020-12-08 13:11:17 -0500

    Prueba

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-#Variables dinamicas en el url = < string nombre > / <int id>  // con esto podemos sacar el id de la imagen y el usuario asociado a la imagen
+# Variables dinamicas en el url = < string nombre > / <int id>  // con esto podemos sacar el id de la imagen y el usuario asociado a la imagen
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods = ['GET'])
+
+@app.route('/login', methods=['GET'])
 def login():
     return render_template('ingreso.html')
 
-@app.route('/registro', methods = ['GET', 'POST'])
+
+@app.route('/registro', methods=['GET', 'POST'])
 def registro():
     return render_template('registro.html')
 
 
-@app.route('/nosotros', methods = ['GET'])
+@app.route('/nosotros', methods=['GET'])
 def nosotros():
     return render_template('Nosotros.html')
 
-@app.route('/perfil', methods = ['GET'])
+
+@app.route('/perfil', methods=['GET'])
 def perfil():
     return render_template('perfil.html')
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
