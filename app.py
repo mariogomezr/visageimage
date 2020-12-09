5fb3b227884a23a7f5454e600a5fe1776427a5e6
Nicodez <n98hernandezv@gmail.com>
2020-12-09 18:01:07 -0500

    Se modifica la vista olvidaPass para que valide pero tiene un error al momento de ingresar el email y pulsar el boton, toca corregir

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_wtf import form
-from forms import CambiaPassForm, LoginForm, RegistroForm
+from forms import CambiaPassForm, LoginForm, OlvidaPassForm, RegistroForm
 
 
 app = Flask(__name__)
@@ -16,17 +16,18 @@ def index():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()                  #Se instancia el objeto form --> WTForm
-    if form.validate_on_submit():       #Se valida si el usuario envia un metodo POST con los datos validos            
+    form = LoginForm()  # Se instancia el objeto form --> WTForm
+    if form.validate_on_submit():  # Se valida si el usuario envia un metodo POST con los datos validos
         if form.submit.data:
-            usuario = form.usuario.data         #Con estos datos se valida en la BD si esta registrado
+            usuario = form.usuario.data  # Con estos datos se valida en la BD si esta registrado
             contrasena = form.contrasena.data
-            #FALTA funcion chequear que el usuario este en la BD
-            next = request.args.get('next', None)   #Si lo esta tal accion
+            # FALTA funcion chequear que el usuario este en la BD
+            next = request.args.get('next', None)  # Si lo esta tal accion
             if next:
                 return redirect(next)
-        return render_template("index.html")    #sino esta accion
-    return render_template("ingreso.html", form = form) # Solo es ejecuta en el metodo GET
+        return render_template("index.html")  # sino esta accion
+    # Solo es ejecuta en el metodo GET
+    return render_template("ingreso.html", form=form)
 
 
 @app.route('/registro', methods=['GET', 'POST'])
@@ -35,6 +36,14 @@ def registro():
     if form.validate_on_submit():
         return render_template("perfil.html")
     return render_template("registro.html", form=form)
+
+
+@app.route('/olvidaPass', methods=['GET', 'POST'])
+def olvidaPass():
+    form = OlvidaPassForm()
+    if form.validate_on_submit():
+        return render_template('ingreso.html')
+    return render_template('olvidaPass.html', form=form)
 
 
 @app.route('/cambiaPass', methods=['GET', 'POST'])
@@ -50,18 +59,22 @@ def nosotros():
     return render_template('Nosotros.html')
 
 
-@app.route('/perfil', methods=['GET', 'POST'])  # Dejar en POST para que cuando se inicie sesión se redireccione al perfil
+# Dejar en POST para que cuando se inicie sesión se redireccione al perfil
+@app.route('/perfil', methods=['GET', 'POST'])
 def perfil():
-    
+
     return render_template('perfil.html')
 
-@app.route('/subirimagen', methods=['GET','POST'])
+
+@app.route('/subirimagen', methods=['GET', 'POST'])
 def subirimagen():
     return render_template('SubirImagen.html')
+
 
 @app.route('/terminos', methods=['GET'])
 def terminos():
     return render_template('termsCond.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -3,20 +3,28 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
+
 class LoginForm (FlaskForm):
-    usuario = StringField('Usuario', validators = [DataRequired(), Length(max = 64)] )
-    contrasena = PasswordField('Contraseña ', validators = [DataRequired(), ] )
-    submit = SubmitField('Iniciar sesión')  #render_kw={"onclick": "{{ url_for('registro') }}"  esto sirve para darle una funcion cuando de click al boton
+    usuario = StringField('Usuario', validators=[DataRequired(), Length(max=64)])
+    contrasena = PasswordField('Contraseña ', validators=[DataRequired(), ])
+    # render_kw={"onclick": "{{ url_for('registro') }}"  esto sirve para darle una funcion cuando de click al boton
+    submit = SubmitField('Iniciar sesión')
+
 
 class RegistroForm(FlaskForm):
-    nombre = StringField('Nombre completo', validators = [DataRequired(), Length(max=64)])
+    nombre = StringField('Nombre completo', validators=[DataRequired(), Length(max=64)])
     usuario = StringField('Usuario', validators=[DataRequired(), Length(max=64)])
     email = StringField('Correo electrónico', validators=[DataRequired(), Length(max=64)])
-    contrasena = PasswordField('Contraseña ', validators = [DataRequired(),])
+    contrasena = PasswordField('Contraseña ', validators=[DataRequired(), ])
     submit = SubmitField('Registrarse')
+
 
 class CambiaPassForm(FlaskForm):
     contrasena = PasswordField('Nueva contraseña', validators=[DataRequired(), EqualTo('confirmar', message='Las contraseñas deben coincidir')])
     confirmar = PasswordField('Repite la contraseña', validators=[DataRequired()])
-    
+    submit = SubmitField('Cambiar')
 
+
+class OlvidaPassForm(FlaskForm):
+    email = StringField('Correo electrónico', validators=[DataRequired(), Length(max=64)])
+    submit = SubmitField('Enviar')
