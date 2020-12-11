087583db3df694041f5ca0240efa7c477ca159cc
Mario <mariogomezr96@gmail.com>
2020-12-10 22:14:54 -0500

    Funcionalidad de login con usuario de prueba

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_wtf import form
 from forms import CambiaPassForm, LoginForm, OlvidaPassForm, RegistroForm
-
+import sys
+import logging
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
+#logging.basicConfig(filename='demo.log', level=logging.DEBUG)
+
+class usuario():
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
 # Variables dinamicas en el url = < string nombre > / <int id>  // con esto podemos sacar el id de la imagen y el usuario asociado a la imagen
 
+
+prueba = { 
+    'username' : 'magio',
+    'password' : '1234'
+}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,16 +37,18 @@ def index():
 def login():
     form = LoginForm()  # Se instancia el objeto form --> WTForm
     if form.validate_on_submit():  # Se valida si el usuario envia un metodo POST con los datos validos
-        if form.submit.data:
             usuario = form.usuario.data  # Con estos datos se valida en la BD si esta registrado
             contrasena = form.contrasena.data
-            # FALTA funcion chequear que el usuario este en la BD
-            next = request.args.get('next', None)  # Si lo esta tal accion
-            if next:
-                return redirect(next)
-        return render_template("index.html")  # sino esta accion
-    # Solo es ejecuta en el metodo GET
-    return render_template("ingreso.html", form=form)
+            #app.logger.info( usuario + '------' + contrasena)  DEBUG
+            if prueba['username'] == usuario and prueba['password'] == contrasena:
+                #app.logger.info('ingreso sucessful')  DEBUG
+                flash('Ingreso exitoso')
+                return render_template("index.html")
+            else: 
+                flash('Intente de nuevo')
+                #app.logger.info('Credenciales incorrectas') debug
+    
+    return render_template("ingreso.html", form=form)  # Solo es ejecuta en el metodo GET
 
 
 @app.route('/registro', methods=['GET', 'POST'])
