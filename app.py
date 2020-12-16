78cc5e0c90164370b5381e6968d07996961949c7
Nicodez <n98hernandezv@gmail.com>
2020-12-16 17:11:26 -0500

    Se agrega una linea que solventa el error de <<Working outside of application context>>

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ UPLOAD_FOLDER = r'static\uploaded_imgs'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.update(
